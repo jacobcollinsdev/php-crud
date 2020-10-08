9f6c8114b852d573c039e6b330851d788f55f4e5
Jacob <jacobc1596@gmail.com>
2020-10-08 11:51:15 +0100

    Created session messages & redirects

--- index.php
+++ index.php
@@ -11,6 +11,22 @@
 </head>
 <body>
     <?php require_once('process.php');?>
+
+    <?php 
+        if(isset($_SESSION['message'])): 
+    ?>
+
+    <div class="alert alert-<?=$_SESSION['msg_type']?>">
+    
+        <?php
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        ?>
+    </div>
+    <?php endif ?>
+
+
+
     <div class="container">
         <?php
             // CONNECT TO DATABASE
--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 <?php
-
+    session_start();
     //CREATE CONNECTION TO DATABASE OR THROW ERROR IF CONNECTION FAILED
     $db = new mysqli('localhost', 'root', 'cuvxus', 'crud') or die("Connection failed: ".$db->connect_error);
 
@@ -10,12 +10,25 @@
 
         $db->query("INSERT INTO data (name, location) VALUES('$name', '$location')") 
         or die($db->error);
+
+        // SET SESSION VARIABLES
+        $_SESSION['message'] = 'Record has been saved!';
+        $_SESSION['msg_type'] = 'success';
+
+        //REDIRECT TO index.php
+        header("location: index.php");
     }
 
     //CHECK IF DELETE BUTTON HAS BEEN CLICKED
     if(isset($_GET['delete'])){
         $id = $_GET['delete'];
+
         $db->query("DELETE FROM data WHERE id=$id") or die($db->error());
+
+        $_SESSION['message'] = 'Record has been deleted!';
+        $_SESSION['msg_type'] = 'danger';
+
+        header("location: index.php");
     }
 
 
