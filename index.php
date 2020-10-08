0515a53772e938d56398574bb7290d9ca9f0890a
Jacob <jacobc1596@gmail.com>
2020-10-08 14:07:02 +0100

    Added alert dismissing

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
         echo $_SESSION['message'];
         unset($_SESSION['message']);
     ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
     <?php endif ?>
 
